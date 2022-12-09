afe1bfc6f1199ddf159e7b81137a04da72758ff5
wicheror <84172776+wicheror@users.noreply.github.com>
2022-12-09 18:51:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,67 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+// Your YouTube API key
+const API_KEY = "AIzaSyDfASpJPAzrhiYLRL5vyv4FAvAXdIdsgnk";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// The ID of the YouTube channel to search for videos
+const CHANNEL_ID = "UC_CHANNEL_ID";
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+// The maximum number of videos to search for
+const MAX_RESULTS = 10;
 
-      <Footer />
-    </div>
-  )
+// An array to store the search results
+const videos = [];
+
+// Initialize the YouTube Data API client
+const client = gapi.client.init({
+  apiKey: API_KEY
+});
+
+// Search for videos on the specified channel
+function searchVideos() {
+  return client.youtube.search.list({
+    part: "id",
+    channelId: CHANNEL_ID,
+    type: "video",
+    maxResults: MAX_RESULTS
+  });
 }
+
+// Get the details of the search results
+function getVideoDetails(searchResults) {
+  const videoIds = searchResults.map(result => result.id.videoId).join(",");
+  return client.youtube.videos.list({
+    part: "snippet,contentDetails",
+    id: videoIds
+  });
+}
+
+// Store the search results in the videos array
+function storeResults(results) {
+  videos.push(...results);
+}
+
+// Select a random video from the search results
+function getRandomVideo() {
+  const randomIndex = Math.floor(Math.random() * videos.length);
+  return videos[randomIndex];
+}
+
+// Embed the selected video on the page
+function playVideo(video) {
+  const player = new YT.Player("player", {
+    videoId: video.id,
+    events: {
+      onReady: function(event) {
+        event.target.playVideo();
+      }
+    }
+  });
+}
+
+// Initialize the YouTube Data API client and search for videos
+gapi.load("client", () => {
+  searchVideos()
+    .then(getVideoDetails)
+    .then(storeResults)
+    .then(getRandomVideo)
+    .then(playVideo);
+});
