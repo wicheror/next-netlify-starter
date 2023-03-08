7fbc723cbae79da11678d4869efc106937aef3ad
wicheror <84172776+wicheror@users.noreply.github.com>
2023-03-08 21:13:31 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,11 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+import React from "react"
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: "white", padding: "1rem" }}>
+      <h1 style={{ margin: 0, fontFamily: "Arial, sans-serif", fontSize: "2rem" }}>Nicolas Wicher</h1>
+    </header>
+  )
 }
+
+export default Header
